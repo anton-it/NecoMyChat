6b9f85cd7f1206ffefeba5310ae5b03ceeec7837
Admin1 <a89086829981@gmail.com>
2023-09-04 14:52:57 +0300

    step1 - Add UI Button, TextView, EditText - Added functionality

--- app/src/main/java/com/ak87/necomychat/MainActivity.kt
+++ app/src/main/java/com/ak87/necomychat/MainActivity.kt
@@ -2,22 +2,49 @@ package com.ak87.necomychat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.FirebaseDatabase
+import com.ak87.necomychat.databinding.ActivityMainBinding
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
         // Write a message to the database
         //
-        val database = Firebase.database("https://mychat-9ff6d-default-rtdb.firebaseio.com")
+        val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        with(binding) {
+            btnSent.setOnClickListener {
+                myRef.setValue(etMessage.text.toString())
+            }
+        }
+
+        onChangeListener(myRef)
+
+    }
+
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object  : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    tvMessage.append("\n")
+                    tvMessage.append(snapshot.value.toString())
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
